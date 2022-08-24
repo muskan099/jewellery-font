6e56d7ed1663f65e018181d8cd591d15c53abb08
IshitaQuest <ishitarathore@questglt.org>
2022-08-24 12:41:37 +0530

    transaction,stakes

--- nft/src/pages/Stakes.jsx
+++ nft/src/pages/Stakes.jsx
@@ -4,65 +4,58 @@ import { Row, Col, Modal, Button} from "react-bootstrap";
 import Table from "react-bootstrap/Table";
 import Layout from '../Components/Layout';
 
-function Stakes() {
+function stakes() {
   return (
 
     <Layout>
 <div>
- <div className="transactions-section">
-   <Container fluid className="p-0">
-    <Row>
-        <Col md="3" sm="4">
-            <div className="transaction-side-image">
-               <img src="images/detail-img.png" alt="" className="img-fluid"/>
-            </div>
-        </Col> 
-        <Col md="9" sm="8">
-                <div className="transaction-box">
+          <div className="stakes-section">
+            <Container>
+              <Row>
+                <Col md="12">
+                  <div className="stakes">
                     <div className="for-image-box1">
-                        <img  className="m-0 img-fluid" src="assets/images/detail-img.png" alt=""/>
-                        <p className="paragraph-main1 py-2">0x800d9250b9f8f46ef8b</p>
+                        <img src="assets/images/detail-img.png" alt=""/>
+                        <p className="paragraph-main1 py-2">0x800d9250b9f8f46eef7317988c4b547b781f1af170f8b</p>
                     </div>
                     <div className="table-responsive">
-                      <table className="table transactions-table-new table-order">
+                      <table className="table  table-details ">
                         <thead>
                           <tr className="for-back">
-                            <th width="2%">S.No</th>
-                            <th width="6%">Image</th>
-                            <th width="8%">Name</th>
-                            <th width="7%">Buying Price</th>
-                            <th width="7%">Creator Address</th>
-                            <th width="7%">Transaction hash</th>
-                            <th width="5%">Token</th>
-                            <th width="5%">Status</th>
-                            <th width="18%">Action</th>
+                            <th width="10%">Wallet Address</th>
+                            <th width="8%">Amount</th>
+                            <th width="10%">Total Interest</th>
+                            <th width="10%">Total Amount</th>
+                            <th width="7%">Start Date</th>
+                            <th width="10%">Lockup End</th>
+                            <th width="7%">Status</th>
+                            <th width="7%">Action</th>
+                            <th width="7%">APY(%)</th>
                           </tr>
                           </thead>
                           <tbody>
                                 <tr className="for-body-tr">
-                                  <td  className="td-break">1</td>
-                                  <td  className="td-break"><img  className="img-fluid" src="images/detail-img.png" width="40px" alt=""/></td>
-                                  <td className="td-break">Hello</td>
-                                  <td  className="td-break">35000</td>
-                                  <td  className="td-break">0*810214555455454545</td>
-                                  <td  className="td-break">0*5654114444444555</td>
-                                  <td  className="td-break">9160</td>
-                                  <td className="td-break success-green">success</td>
-                                  <td className="td-break"><button className="btn-sell1">Sell</button>
-                                  <button className="btn-sell1">Cancel</button><button className="btn-sell1">Auction</button>
-                                  </td>
+                                  <td className="td-break">65454545</td>
+                                  <td  className="td-break">1200</td>
+                                  <td  className="td-break">4,700</td>
+                                  <td className="td-break">1204.7340</td>
+                                  <td  className="td-break">2022-07-20</td>
+                                  <td className="td-break">2022-11-09</td>
+                                  <td className="td-break"><button className="btn-active">Active</button></td>
+                                  <td className="td-break"><button className="btn-sell1">Closed</button></td>
+                                  <td className="td-break">12</td>
                                 </tr>
                         </tbody>
                       </table>
                     </div>
-                </div>
-        </Col>
-    </Row>
-   </Container>
- </div>
-</div>
-</Layout>
+                  </div>
+                </Col>
+              </Row>
+            </Container>
+          </div>
+        </div>
+        </Layout>
     );
 }
 
-export default Stakes;
+export default stakes;
