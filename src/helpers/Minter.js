dbc92b6db31a31be77425f19bd5121ada144ebba
aashishquest <aashishporwal@questglt.org>
2022-12-05 12:39:21 +0530

    minter

--- src/helpers/Minter.js
+++ src/helpers/Minter.js
@@ -1,14 +1,13 @@
 const { ethers } = require("ethers");
 
 // These constants must match the ones used in the smart contract.
-const SIGNING_DOMAIN_NAME = "Jewellery NFT";
+const SIGNING_DOMAIN_NAME = "JewelleryNFT";
 const SIGNING_DOMAIN_VERSION = "1";
 
 /**
  * JSDoc typedefs.
  *
  * @typedef {object} NFTVoucher
- * @property {ethers.BigNumber | number} tokenId the id of the un-minted NFT
  * @property {ethers.BigNumber | number} minPrice the minimum price (in wei) that the creator will accept to redeem this NFT
  * @property {string} uri the metadata URI to associate with this NFT
  * @property {ethers.BytesLike} signature an EIP-712 signature of all fields in the NFTVoucher, apart from signature itself.
@@ -33,18 +32,16 @@ export class LazyMinter {
   /**
    * Creates a new NFTVoucher object and signs it using this LazyMinter's signing key.
    *
-   * @param {ethers.BigNumber | number} tokenId the id of the un-minted NFT
    * @param {string} uri the metadata URI to associate with this NFT
    * @param {ethers.BigNumber | number} minPrice the minimum price (in wei) that the creator will accept to redeem this NFT. defaults to zero
    *
    * @returns {NFTVoucher}
    */
-  async createVoucher(tokenId, uri, minPrice) {
-    const voucher = { tokenId, uri, minPrice };
+  async createVoucher(uri, minPrice) {
+    const voucher = {uri, minPrice };
     const domain = await this._signingDomain();
     const types = {
       NFTVoucher: [
-        { name: "tokenId", type: "uint256" },
         { name: "minPrice", type: "uint256" },
         { name: "uri", type: "string" },
       ],
